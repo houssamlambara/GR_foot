888f8be5bc62150e95ec5a720c8f689e3ff50f1b
houssamlambara <lambarahoussam@gmail.com>
2025-04-10 16:28:33 +0100

    ajout et modification des routes pour ajouter terrain

--- GR_foot/routes/web.php
+++ GR_foot/routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\TerrainController;
+use App\Http\Controllers\TournoiController;
+use App\Http\Controllers\UtilisateurController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,33 +18,57 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Routes publiques
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/reservation', [PageController::class, 'reservation'])->name('reservation')->middleware('auth');
 
 // Routes principales
-Route::get('/index', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/activiter', [PageController::class, 'activiter'])->name('activiter')->middleware('auth');
-Route::get('/addterrain', [PageController::class, 'addterrain']);
-Route::get('/addtounois', [PageController::class, 'addtounois']);
-Route::get('/contact', [PageController::class, 'contact']);
-Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard')->middleware('auth');
-Route::get('/dashboardReservation', [PageController::class, 'dashboardReservation']);
-Route::get('/reservation', [PageController::class, 'reservation'])->middleware("auth");
+Route::middleware(['auth'])->group(function () {
+    Route::get('/index', [PageController::class, 'index']);
+    Route::get('/activiter', [PageController::class, 'activiter'])->name('activiter');
+    Route::get('/addtounois', [PageController::class, 'addtounois']);
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+    Route::get('/dashboardReservation', [PageController::class, 'dashboardReservation'])->name('dashboardReservation');
+    Route::get('/tournois', [PageController::class, 'tournois']);
+    Route::get('/addterrain', [PageController::class, 'addterrain'])->name('addterrain');
+    Route::get('/utilisateur', [PageController::class, 'utilisateur'])->name('utilisateurs.index');
+});
+
+// Routes d'authentification
 Route::get('/signin', [PageController::class, 'signin'])->name('signin');
-Route::get('/tournois', [PageController::class, 'tournois'])->middleware('auth');
-Route::get('/utilisateur', [PageController::class, 'utilisateur']);
-
-// Register
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-
-// Route::get('/login', function () {
-//     return view('dashboard');
-// })->middleware('auth')->name('dashboard');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Routes pour la gestion des tournois
+Route::middleware(['auth', 'Admin'])->group(function () {
+    Route::get('/tournois', [TournoiController::class, 'index'])->name('tournois.index');
+    Route::get('/tournois/create', [TournoiController::class, 'create'])->name('tournois.create');
+    Route::post('/tournois', [TournoiController::class, 'store'])->name('tournois.store');
+    Route::get('/tournois/{tournoi}/edit', [TournoiController::class, 'edit'])->name('tournois.edit');
+    Route::put('/tournois/{tournoi}', [TournoiController::class, 'update'])->name('tournois.update');
+    Route::delete('/tournois/{tournoi}', [TournoiController::class, 'destroy'])->name('tournois.destroy');
+});
+
+// Routes pour la gestion des terrains (protégées pour les administrateurs)
+Route::middleware(['auth', 'Admin'])->group(function () {
+    Route::get('/terrains', [TerrainController::class, 'index'])->name('terrains.index');
+    Route::get('/terrains/create', [TerrainController::class, 'create'])->name('terrains.create');
+    Route::post('/terrains', [TerrainController::class, 'store'])->name('terrains.store');
+    Route::get('/terrains/{terrain}/edit', [TerrainController::class, 'edit'])->name('terrains.edit');
+    Route::put('/terrains/{terrain}', [TerrainController::class, 'update'])->name('terrains.update');
+    Route::delete('/terrains/{terrain}', [TerrainController::class, 'destroy'])->name('terrains.destroy');
+});
+
+// Routes pour la gestion des utilisateurs
+Route::middleware(['auth', 'Admin'])->group(function () {
+    Route::get('/utilisateurs', [UtilisateurController::class, 'index'])->name('utilisateurs.index');
+    Route::get('/utilisateurs/create', [UtilisateurController::class, 'create'])->name('utilisateurs.create');
+    Route::post('/utilisateurs', [UtilisateurController::class, 'store'])->name('utilisateurs.store');
+    Route::get('/utilisateurs/{utilisateur}/edit', [UtilisateurController::class, 'edit'])->name('utilisateurs.edit');
+    Route::put('/utilisateurs/{utilisateur}', [UtilisateurController::class, 'update'])->name('utilisateurs.update');
+    Route::delete('/utilisateurs/{utilisateur}', [UtilisateurController::class, 'destroy'])->name('utilisateurs.destroy');
+});
+
