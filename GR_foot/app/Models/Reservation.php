5a2d3fe1969ee0b560fd5d52cdf9a2ac4436da32
houssamlambara <lambarahoussam@gmail.com>
2025-04-11 15:28:05 +0100

    modification du modal reservation

--- GR_foot/app/Models/Reservation.php
+++ GR_foot/app/Models/Reservation.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'terrain_id',
+        'date',
+        'heure_debut',
+        'heure_fin',
+        'montant',
+        'disponibilite',
+        'telephone'
+    ];
+
+    // Relation avec le terrain
+    public function terrain()
+    {
+        return $this->belongsTo(Terrain::class);
+    }
+
+    // Relation avec l'utilisateur
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
